ce0264a8dfe9bccd1d7be9848abe04e46cf10fb5
Raniel Santos <raniel@forlogic.net>
2021-04-28 07:44:32 -0300

    Correções

--- front-end/desafio-full/src/app/app-routing.module.ts
+++ front-end/desafio-full/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/debts', pathMatch: 'full' },
   { path: 'debts', loadChildren: () => import('./debts/debt.module').then(m => m.DebtModule) },
 ];
 
